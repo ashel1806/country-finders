fbee07c20c339dec0c4a7a6d52b6913bf69a4399
Ashel Vasquez <vasquezpalominoashel@gmail.com>
2022-02-05 22:58:30 -0500

    Adding ESlint

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+  'env': {
+    'commonjs': true,
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime'
+  ],
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
+    },
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react'
+  ],
+  'settings': {
+    'react': {
+      'version': 'detect',
+    },
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+      'error', 'always'
+    ],
+    'arrow-spacing': [
+      'error', { 'before': true, 'after': true }
+    ],
+    'no-console': 0
+  }
+};
